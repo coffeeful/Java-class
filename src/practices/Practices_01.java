663a1015ffa131d778a02fc54f6ab0d0cefd719c
coffeeful <dltjdgus03@gmail.com>
2023-11-30 13:29:13 +0900

    이성현 정보

---
+++ src/practices/Practices_01.java
@@ -0,0 +1,19 @@
+package practices;
+
+public class Practices_01 {
+
+	public static void main(String[] args) {
+		// 본인의 이름(name), 나이(age), 키(height), 주소(address), 전화번호 (mobile)
+		//를 입력할 수 있는 변수를 각각 선언해봅시다.
+		
+		String name = "이름 (이성현)";
+				int age = 32;
+				int height = 183;
+				String address = "주소(인천 부평구)";
+				String mobile = "전화번호(010-1234-5678)";
+				
+				
+
+	}
+
+}
