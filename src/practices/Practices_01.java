7d660d3975b79424f86d306f91d331366d0c6724
coffeeful <dltjdgus03@gmail.com>
2023-11-30 13:33:36 +0900

    이성현 정보

--- src/practices/Practices_01.java
+++ src/practices/Practices_01.java
@@ -6,7 +6,7 @@ public class Practices_01 {
 		// 본인의 이름(name), 나이(age), 키(height), 주소(address), 전화번호 (mobile)
 		//를 입력할 수 있는 변수를 각각 선언해봅시다.
 		
-		String name = "이름 (이성현)";
+				String name = "이름 (이성현)";
 				int age = 32;
 				int height = 183;
 				String address = "주소(인천 부평구)";
