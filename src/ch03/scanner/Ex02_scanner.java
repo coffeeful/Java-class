ba65edcebbde21f9024fcc7d79a81999f52c8d8d
coffeeful <dltjdgus03@gmail.com>
2023-12-01 11:39:20 +0900

    연산자  연습

---
+++ src/ch03/scanner/Ex02_scanner.java
@@ -0,0 +1,21 @@
+package ch03.scanner;
+
+import java.util.Scanner;
+
+public class Ex02_scanner {
+
+	public static void main(String[] args) {
+		/**
+		 * 정수 2개를 각각 스캐너로 num1, nu2에 입력 받아서 입력받은 수의 합을 출력하세요.
+		 */
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요: ");
+				int num1 = scanner.nextInt();
+				int num2 = scanner.nextInt();
+				int num3= (num1 + num2);
+				
+					
+		System.out.println( num1 + "과(와)" +  num2 +  "의 합은" +  num3  + "입니다");
+	}
+
+}
