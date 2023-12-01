1261c924eea302b2e61f4c20d3264ec5a68be98d
coffeeful <dltjdgus03@gmail.com>
2023-12-01 13:16:02 +0900

    스캐너 추가

--- src/ch03/scanner/Ex02_scanner.java
+++ src/ch03/scanner/Ex02_scanner.java
@@ -10,12 +10,23 @@ public class Ex02_scanner {
 		 */
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요: ");
-				int num1 = scanner.nextInt();
-				int num2 = scanner.nextInt();
-				int num3= (num1 + num2);
-				
-					
-		System.out.println( num1 + "과(와)" +  num2 +  "의 합은" +  num3  + "입니다");
-	}
+//				int num1 = scanner.nextInt();
+//				int num2 = scanner.nextInt();
+//				int num3= (num1 + num2);
+//				
+//					
+//		System.out.println( num1 + "과(와)" +  num2 +  "의 합은" +  num3  + "입니다");
+	
+		System.out.println("num1의 값:  "); int num1 = scanner.nextInt();
+		System.out.println("num2의 값:  "); int num2 = scanner.nextInt();
+		
+	
+		
+		int sub = (num1 - num2);
+		System.out.println(num1 + " 과(와)  " +  num2 + " 의 빼기는 " + sub + "입니다");
 
+		
+}
+
+	
 }
