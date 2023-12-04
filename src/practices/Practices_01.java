99621fbd6a3763e8f7660ce96bb2b64ce5f27b08
coffeeful <dltjdgus03@gmail.com>
2023-12-04 15:56:18 +0900

    For case

--- src/practices/Practices_01.java
+++ src/practices/Practices_01.java
@@ -1,5 +1,7 @@
 package practices;
 
+import java.util.Scanner;
+
 public class Practices_01 {
 
 	public static void main(String[] args) {
@@ -9,22 +11,25 @@ public class Practices_01 {
 		// String; // x 타입만 주면 안됨
 		// name = "이름"; // x 변수이름만 주면 안됨
 		
-				String name = "이성현";
-				int age = 32;
-				double height = 182.7;
-				String address = " 인천 부평구";
-				String mobile = "010-1234-5678";
-				
-				//변수값을 출력
-				
-				System.out.println("저의 이름은 : " + name +  " 입니다.");
-				System.out.println("저의 나이는 : " + age +  " 입니다.");
-				System.out.println("저의 키는 : " + height +  " 입니다.");
-				System.out.println("저의 사는곳은 : " + address +  " 입니다.");
-				System.out.println("저의 핸드폰 번호는 : "+ mobile +  " 입니다.");
-				
-				
-				
+//				String name = "이성현";
+//				int age = 32;
+//				double height = 182.7;
+//				String address = " 인천 부평구";
+//				String mobile = "010-1234-5678";
+//
+//				//변수값을 출력
+//
+//				System.out.println("저의 이름은 : " + name +  " 입니다.");
+//				System.out.println("저의 나이는 : " + age +  " 입니다.");
+//				System.out.println("저의 키는 : " + height +  " 입니다.");
+//				System.out.println("저의 사는곳은 : " + address +  " 입니다.");
+//				System.out.println("저의 핸드폰 번호는 : "+ mobile +  " 입니다.");
+
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hello World!");
+
 
 	}
 
